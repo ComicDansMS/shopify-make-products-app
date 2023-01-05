a91a31fce73c902f982063c6f4332250098215ec
Daniel Holmes <dan.r.holmes@gmail.com>
2023-01-05 17:18:58 +1030

    add sass and make card

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppBridgeProvider, QueryProvider } from "./components";
 import HomePage from "./pages/HomePage";
+import './css/app.scss';
 
 export default function App() {
   return (
